9f91f9ea7af9b4768f2a8fdabac846c88c67899e
Izzy Gibson <izzyyg27@gmail.com>
2024-01-25 19:45:43 -0800

    Add nicer formatting

--- .docker/ubuntu/refresh_ip_address.sh
+++ .docker/ubuntu/refresh_ip_address.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This script is used to refresh the IP address of the seedbox on MAM
+
+clear
+
 while true;
 do
     if ! command -v curl &> /dev/null; then
@@ -7,5 +11,6 @@ do
         apt-get install curl -y
     fi
     curl -c config/mam.cookies -b "mam_id=$MAM_TOKEN" -o config/mam.log https://t.myanonamouse.net/json/dynamicSeedbox.php
+    echo "[$(date)] $(cat config/mam.log)"
     sleep 3600
 done
