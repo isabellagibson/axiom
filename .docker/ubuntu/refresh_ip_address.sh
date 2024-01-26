6995179166c8cb0ce5fbfe947704ac327d1ed01d
Izzy Gibson <izzyyg27@gmail.com>
2024-01-25 19:21:49 -0800

    Update refresh_ip_address.sh

--- .docker/ubuntu/refresh_ip_address.sh
+++ .docker/ubuntu/refresh_ip_address.sh
@@ -6,6 +6,6 @@ do
         apt-get update -y
         apt-get install curl -y
     fi
-    curl -c ~/mam.cookies -b 'mam_id=$MAM_TOKEN' https://t.myanonamouse.net/json/dynamicSeedbox.php
+    curl -c mam.cookies -b 'mam_id=$MAM_TOKEN' -o mam.log https://t.myanonamouse.net/json/dynamicSeedbox.php
     sleep 3600
 done
