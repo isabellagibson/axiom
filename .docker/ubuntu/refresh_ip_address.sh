f4508e82c8bf8d144a581daf34d984ef53887059
Izzy Gibson <izzyyg27@gmail.com>
2024-01-27 23:02:11 -0800

    Add bonus points spending (MAM)

--- .docker/ubuntu/refresh_ip_address.sh
+++ .docker/ubuntu/refresh_ip_address.sh
@@ -2,15 +2,18 @@
 
 # This script is used to refresh the IP address of the seedbox on MAM
 
+# Download curl if not installed
+if ! command -v curl &> /dev/null; then
+        apt-get update -y
+        apt-get install curl -y
+fi
+
 clear
 
 while true;
 do
-    if ! command -v curl &> /dev/null; then
-        apt-get update -y
-        apt-get install curl -y
-    fi
-    curl -c config/mam.cookies -b "mam_id=$MAM_TOKEN" -o config/mam.log https://t.myanonamouse.net/json/dynamicSeedbox.php
-    echo "[$(date)] $(cat config/mam.log)"
+    curl -c config/mam.cookies -b "mam_id=$MAM_TOKEN" -o config/mam_bonus_points_spent.log -s "https://www.myanonamouse.net/json/bonusBuy.php/?spendtype=upload&amount=Max%20Affordable%20"
+    curl -c config/mam.cookies -b "mam_id=$MAM_TOKEN" -o config/mam_ip_refresh.log -s "https://t.myanonamouse.net/json/dynamicSeedbox.php"
+    echo "[$(date)] $(cat mam_ip_refresh/mam.log)"
     sleep 3600
 done
